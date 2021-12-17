00f6e40fb0efef61ffead5db0f3b8ca5673e3089
jigglyjams <cousinjeffrey@protonmail.com>
2021-12-16 22:01:37 -0800

    GuildId issue

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ discord.on('messageCreate', async m => {
 
     const userId = m.author.id;
     const guildId = m.guildId;
+    console.log(guildId);
     const username = `${m.author.username}#${m.author.discriminator}`;
 
     if (m.content.indexOf('!gm setup') == 0) {
